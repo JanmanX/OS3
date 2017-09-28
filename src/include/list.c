819358a54ce4c5ff9a65232afec72f768f9ff109
Jan Meznik <jan@meznik.dk>
2017-09-28 07:37:26 +0200

    Fixed lists

--- src/include/list.c
+++ src/include/list.c
@@ -2,10 +2,15 @@
 #include <libc.h>
 #include <mem/mem.h>
 
-list *list_create(void)
+list_t* list_create(void)
 {
         list_t *list = malloc(sizeof(list_t));
         ASSERT(list != NULL, "Failed to malloc memory");
+
+	list->first = NULL;
+	list->last = NULL;
+	list->count = 0x00;
+
         return list;
 }
 
@@ -27,72 +32,102 @@ void list_destroy(list_t *list)
 
 void list_clear(list_t *list)
 {
-        LIST_FOREACH_NODE(list, first, next, cur)
+        LIST_FOREACH_NODE(list, cur)
         {
                 free(cur->value);
                 cur->value = NULL;
         }
 }
 
-void list_push(list_t *list, void *value)
+list_node_t* list_push(list_t *list, void *value)
 {
+
         list_node_t *node = malloc(sizeof(list_node_t));
 	ASSERT(node != NULL, "Malloc returned NULL");
 
         node->value = value;
-        if(list->last == NULL)
-        {
-                list->first = node;
-                list->last = node;
-        }
-        else
-        {
-                list->last->next = node;
-                node->prev = list->last;
-                list->last = node;
-        }
+	node->next = NULL; /* It is pushed, so its always last element */
+	list->last = node;
 
-        list->count++;
+	if(list->first == NULL) {
+		list->first = node;
+		node->prev = NULL;
+	} else {
+		/* Iterate to the last element from the start */
+		list_node_t* iter = list->first;
+		while(iter->next != NULL) {
+			iter = iter->next;
+		}
+		iter->next = node;
+		node->prev = iter;
+	}
+
+	list->count++;
+
+	return node;
 }
 
 void *list_pop(list_t *list)
 {
-        ASSERT(list != NULL, "list is NULL");
+	ASSERT(list != NULL, "list is NULL");
 
-        list_node_t *node = list->last;
-        return node != NULL ? list_remove(list, node) : NULL;
+	list_node_t *node = list->last;
+	return node != NULL ? list_remove(list, node) : NULL;
 }
 
 
 void *list_remove(list_t *list, list_node_t *node)
 {
-        ASSERT(list != NULL, "list is NULL");
+	ASSERT(list != NULL, "list is NULL");
 
-        void *result = NULL;
+	void *result = NULL;
 
-        if(node == list->first && node == list->last)
-        {/* If only element in list (both first and last element) */
-                list->first = NULL;
-                list->last = NULL;
-        }
-        else if(node == list->first)
-        {
-                list->first = list->first->next;
-                list->first->prev = NULL;
-        }
-        else if(node == list->last)
-        {
-                list->last = list->last->prev;
-                list->last->next = NULL;
-        }
-        else {/* Else if somewhere in the middle of the list */
-		node->prev->next = node->next;
-		node->next->prev = node->prev;
-        }
+	LIST_FOREACH_NODE(list, cur) {
+		if(cur != node) {
+			continue;
+		}
 
-        list->count--;
-        result = node->value;
-        free(node);
+		/* Element has been found. Remove it from list appropriatelly */
 
-	return result;
+
+		if(node == list->first) {
+			list->first = node->next;
+		}
+
+		if(node == list->last) {
+			list->last = node->prev;
+		}
+
+		if(node->prev != NULL) {
+			node->prev->next = node->next;
+		}
+
+		if(node->next != NULL) {
+			node->next->prev = node->prev;
+		}
+
+		list->count--;
+		result = node->value;
+		free(node);
+
+		return result;
+
+
+	}
+
+	LOG("ITEM NOT FOUND");
+	return NULL;
 }
+
+void *list_find(list_t *list, void *search)
+{
+	LIST_FOREACH_NODE(list, cur) {
+		if(cur->value == search)  {
+			return cur;
+		}
+	}
+
+	return NULL;
+}
+
+
--- src/include/list.h
+++ src/include/list.h
@@ -1,10 +1,9 @@
-#ifndef LIST_H
-#define LIST_H
+#pragma once
 
 typedef struct list_node
 {
-        struct list_node_t *next;
-        struct list_node_t *prev;
+        struct list_node *next;
+        struct list_node *prev;
         void *value;
 } list_node_t;
 
@@ -15,18 +14,33 @@ typedef struct list
         list_node_t *last;
 } list_t;
 
-list *list_create(void);
+/* Creats new list on heap and returns it */
+list_t *list_create(void);
+
+/* Frees the list, as well as all nodes in the list
+ * (does not free the values!) */
 void list_destroy(list_t *list);
+
+/* Frees all values in the list */
 void list_clear(list_t *list);
 
 #define list_count(A) ((A)->count)
 #define list_first(A) ((A)->first != NULL ? (A)->first->value : NULL)
 #define list_last(A) ((A)->last != NULL ? (A)->last->value : NULL)
 
-void list_push(list_t *list, void *value);
+/* Pushes an element to the end of the list */
+list_node_t* list_push(list_t *list, void *value);
+
+/* Removes and returns the last element of the list */
 void *list_pop(list_t *list);
 
+/* Removes an element from list */
 void *list_remove(list_t *list, list_node_t *node);
+
+/* Removes all occurences of an element from list, by value */
+void *list_find(list_t *list, void *search);
+
+
 
 #define LIST_FOREACH_VALUE(L,V) list_node_t *_node;\
 			void* V = NULL;\
@@ -46,4 +60,3 @@ void *list_remove(list_t *list, list_node_t *node);
 for(V = _node = L->S; _node != NULL; V = _node = _node->M)
 */
 
-#endif
