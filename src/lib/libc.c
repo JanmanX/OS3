8bb1540976dccb708d630ebfacd1787b7850a7d2
Jan Meznik <jan@meznik.dk>
2017-09-29 08:03:34 +0200

    Added 16 bit version of port in/out.

--- src/lib/libc.c
+++ src/lib/libc.c
@@ -99,6 +99,14 @@ inline uint8_t inb(uint16_t port)
 	return ret;
 }
 
+inline uint16_t inw(uint16_t port)
+{
+	uint16_t ret;
+	asm volatile ("inw %1, %0"
+		      : "=a"(ret)
+		      : "Nd"(port)
+		     );
+}
 inline uint32_t ind(uint16_t port)
 {
 	uint32_t ret;
@@ -117,10 +125,16 @@ inline void outb(uint16_t port, uint8_t val)
 	asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
 }
 
+inline void outw(uint16_t port, uint16_t val)
+{
+	asm volatile ( "outw %0, %1" : : "a"(val), "Nd"(port) );
+}
+
 inline void outd(uint16_t port, uint32_t val)
 {
 	asm volatile ( "out %0, %1" : : "a"(val), "Nd"(port) );
 }
+
 
 /* CPU-speed independant.
  * From osdev.org */
--- src/lib/libc.h
+++ src/lib/libc.h
@@ -24,9 +24,11 @@ uint8_t checksum_zero(uint8_t *, uint64_t count);
 
 /* Assembly  functions */
 uint8_t inb(uint16_t port);
+uint16_t inw(uint16_t port);
 uint32_t ind(uint16_t port);
 
 void outb(uint16_t port, uint8_t val);
+void outw(uint16_t port, uint16_t val);
 void outd(uint16_t port, uint32_t val);
 
 void io_wait(void);
